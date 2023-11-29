ec1a8ff8fe1867154511c07673d760a4db1f40be
Lixuan <1366070554@qq.com>
2023-11-29 09:43:11 +0800

    修改

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { VueElement, createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue';
-import Login from 'src/components/auth/login.vue'
-const app = createApp(App)
-const login = createApp(Login)
-login.use(Antd)
-app.use(router)
+import Login from './components/auth/login.vue'
 
+
+
+const app = createApp(App).use(Antd).use(router)
 app.mount('#app')
+
